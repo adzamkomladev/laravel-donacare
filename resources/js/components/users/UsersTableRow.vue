ba9ecfc25836bdda1e97b5b1e330bdbbf99ad0c8
adzamkomladev <adzamkomla.dev@gmail.com>
2020-08-07 23:50:47 +0000

    refactor users table row into vuejs component

---
+++ resources/js/components/users/UsersTableRow.vue
@@ -0,0 +1,54 @@
+<template>
+    <tr>
+        <td class="text-left text-muted">
+            <i class="fas fa-user-cog fa-lg"></i>
+            <small>Admin</small>
+        </td>
+        <td class="text-left">Account name</td>
+        <td class="text-left"></td>
+        <td>
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input class="form-check-input" type="checkbox" checked />
+                    <span class="form-check-sign"></span>
+                </label>
+            </div>
+        </td>
+        <td>
+            <span class="badge badge-pill badge-success">Online</span>
+        </td>
+        <td class="td-actions text-right">
+            <button
+                type="button"
+                rel="tooltip"
+                title=""
+                class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral"
+                data-original-title="Edit Task"
+            >
+                <a href="user.html"
+                    ><i class="now-ui-icons users_circle-08"></i
+                ></a>
+            </button>
+            <button
+                type="button"
+                rel="tooltip"
+                title=""
+                class="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral"
+                data-original-title="Remove"
+            >
+                <i
+                    class="now-ui-icons ui-1_simple-remove"
+                    title="delete this user"
+                ></i>
+            </button>
+        </td>
+    </tr>
+</template>
+
+<script>
+export default {
+    mounted() {
+        console.log("Component mounted.");
+    }
+};
+</script>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -21,37 +21,7 @@
                                     <th class="text-right">Action</th>
                                 </thead>
                                 @foreach ($users as $user)
-                                    <tr>
-                                        <td class="text-left text-muted">
-                                            @if($user->role === 'admin')
-                                                <i class="fas fa-user-cog fa-lg"></i>
-                                                <small>Admin</small>
-                                            @endif
-                                        </td>
-                                        <td class="text-left">Account name</td>
-                                        <td class="text-left">{{ $user->telephone }}</td>
-                                        <td>
-                                            @if($user->role !== 'admin')
-                                                <div class="form-check">
-                                                  <label class="form-check-label">
-                                                    <input class="form-check-input" type="checkbox" checked>
-                                                    <span class="form-check-sign"></span>
-                                                  </label>
-                                                </div>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            <span class="badge badge-pill badge-success">Online</span>
-                                        </td>
-                                        <td class="td-actions text-right">
-                                          <button type="button" rel="tooltip" title="" class="btn btn-info btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Edit Task">
-                                            <a href="user.html"><i class="now-ui-icons users_circle-08"></i></a>
-                                          </button>
-                                          <button type="button" rel="tooltip" title="" class="btn btn-danger btn-round btn-icon btn-icon-mini btn-neutral" data-original-title="Remove">
-                                            <i class="now-ui-icons ui-1_simple-remove" title="delete this user"></i>
-                                          </button>
-                                        </td>
-                                    </tr>
+                                    <tr is="users-table-row"></tr>
                                 @endforeach
                             </tbody>
                         </table>
