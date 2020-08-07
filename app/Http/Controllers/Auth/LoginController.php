0164d89f3cb8069ddccd061b6c674ef26eeefc47
adzamkomladev <adzamkomla.dev@gmail.com>
2020-08-07 21:35:36 +0000

    Add user location saving after login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Stevebauman\Location\Facades\Location;
 
 class LoginController extends Controller
 {
@@ -36,6 +38,24 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * The user has been authenticated.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        if ($user->role !== 'admin') {
+            $user_location = Location::get();
+
+            $request->session()->put('user_location', $user_location);
+        }
+
+        return redirect()->route('home');
     }
 
     public function username()
