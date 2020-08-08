cf4e80770e2129ad71904cbd522d1150b2021edf
adzamkomladev <adzamkomla.dev@gmail.com>
2020-08-08 03:22:22 +0000

    add db and model setup for otp feature

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'telephone', 'role', 'password', 'activated'
+        'telephone', 'role', 'password', 'activated', 'otp'
     ];
 
     /**
