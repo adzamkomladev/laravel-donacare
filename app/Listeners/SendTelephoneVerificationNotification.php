1d9f2136ca3cc9d5507627ca15a844cc802cf515
Komla Adzam <adzamkomla.dev@gmail.com>
2020-08-17 01:07:10 +0000

    refactored otp implementation to use SMS.to

--- app/Listeners/SendTelephoneVerificationNotification.php
+++ app/Listeners/SendTelephoneVerificationNotification.php
@@ -27,8 +27,35 @@ class SendTelephoneVerificationNotification
      */
     public function handle(Registered $event)
     {
-        // TODO Setup SMS verification
+        $otp = $event->user->otp;
+        $phone = $event->user->telephone;
+        $tawkToApiKey = env('TAWK_TO_KEY');
 
-        Log::info('Registered user OTP', ['otp' => $event->user->otp]);
+        Log::info('Registered user OTP', ['otp' => $otp, 'phone' => $phone, 'api_key' => $tawkToApiKey]);
+
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.sms.to/sms/send",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => "{\n    \"message\": \"Hello! Your PIN for Blood-donor is {$otp}\",\n    \"to\": \"{$phone}\",\n    \"sender_id\": \"SMSto\",\n    \"callback_url\": \"https://example.com/callback/handler\"\n}",
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type: application/json",
+                "Accept: application/json",
+                "Authorization: Bearer {$tawkToApiKey}"
+            ),
+        ));
+
+        $response = curl_exec($curl);
+
+        Log::info('cURL response', ['response' => $response]);
+
+        curl_close($curl);
     }
 }
