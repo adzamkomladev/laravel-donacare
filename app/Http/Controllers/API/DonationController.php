4c560196f712fd9edd1597ef535e46c68be4db10
Komla Adzam <adzamkomla.dev@gmail.com>
2020-09-06 14:00:24 +0000

    fix donation api error

--- app/Http/Controllers/API/DonationController.php
+++ app/Http/Controllers/API/DonationController.php
@@ -74,11 +74,8 @@ class DonationController extends Controller
             'hospital_name' => 'string|string|max:255',
             'hospital_location' => 'string|string|max:255',
             'description' => 'nullable|string',
-            'date_needed' => 'required|date',
+            'date_needed' => 'required',
             'payment_status' => ['required', Rule::in(['free', 'charged'])],
-            'doctor_name' => 'required|string|max:255',
-            'doctor_phone' => 'required|string|max:15',
-            'doctor_staff_id' => 'required|string|max:40',
             'payment_method' => 'nullable|string',
             'share_location' => 'required|boolean',
             'type' => ['required', Rule::in(['blood', 'organ', 'funds'])]
